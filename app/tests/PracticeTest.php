a9dec8b0fcba751a107dad4c1fab84f21d76243b
sazia <sazia_11yahoo.com>
2014-08-20 10:35:49 +0600

    assert true

---
+++ app/tests/PracticeTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lpc8
+ * Date: 8/20/14
+ * Time: 9:28 AM
+ */
+
+
+class PracticeTest extends PHPUnit_Framework_TestCase{
+
+    public function testItWorks(){
+        $this->assertTrue(false);
+        //$greeting="Hello World";
+
+    }
+}
