abb243b79869693ef81c41d46579d1449397c281
sazia <sazia_11yahoo.com>
2014-08-20 12:15:37 +0600

    variable is a instance of a class

--- app/tests/PracticeTest.php
+++ app/tests/PracticeTest.php
@@ -1,17 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lpc8
- * Date: 8/20/14
- * Time: 9:28 AM
- */
+
 
 
 class PracticeTest extends PHPUnit_Framework_TestCase{
 
     public function testHelloWorld(){
 
-        $greeting="Hello World";
-        $this->assertEqual( $greeting=='Hello,World ',$greeting);
+       // $greeting="Hello World";
+        $this->assertEqual(Actual,optonalmessage);
     }
+    public function testvalue(){
+
+        $val = 0;
+        $this->assertEquals(0, $val);
+    }
+    public function testNullValue(){
+        $val = null;
+         $this->assertSame(0, $val);
+    }
+
+public function testArrayvalue()
+ {
+    $names = ['Taylor', 'Shawn', 'Dayle'];
+         $this->assertContains('Dayle', $names);
+ }
+    public function testArrayNotValue()
+    {
+        $names = ['Taylor', 'Shawn', 'Dayle'];
+         $this->assertNotContains('Troll', $names);
+    }
+    public function testFamilyRequiresParents()
+ {
+          $family = [
+            'parents' => 'Joe',
+              'children' => ['Timmy', 'Suzy']
+                    ];
+
+          $this->assertArrayHasKey('parents', $family);
+            }
+
+    public function testAge(){
+
+              $age=30;
+        $this->assertInternalType('integer',$age);
+
+
+    }
+
+    public function testStampMustBeInstanceOfDateTime()
+ {
+           $date = new DateFormatter(new DateTime);
+
+          $this->assertInstanceOf('DateTime', $date->getStamp());
+    }
+
+
+
 }
